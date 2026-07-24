16d037d11d190256259fc1a9bfc25fcf5781a171
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-07-24 08:27:46 -0300

    Refactor ramais_view to include data preparation for rendering

--- app/views.py
+++ app/views.py
@@ -4,8 +4,19 @@ from django.shortcuts import render
 
 
 def ramais_view(request):
+    data = [
+        {
+            'name': ramal.get_display_name(),
+            'phone': ramal.phone or '',
+            'sector': ramal.sector.name if ramal.sector else '',
+            'machine': ramal.machine or '',
+        }
+        for ramal in Ramais.objects.select_related('sector', 'user').all()
+    ]
+
     return render(request, 'app/ramais.html', {
-        'is_staff': request.user.is_staff
+        'is_staff': request.user.is_staff,
+        'data': data
     })
 
 
