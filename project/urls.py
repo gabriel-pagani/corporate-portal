be554d36589c747a4acf931ca184097b3eccfab4
Gabriel Pagani <gabrielpaganidesouza@gmail.com>
2026-07-26 17:59:01 -0300

    Remove static and media URL patterns from urlpatterns in urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
 
 admin.site.site_header = "Portal de Administração"
 admin.site.site_title = "Sistema"
@@ -11,6 +10,3 @@ urlpatterns = [
     path('', include('app.urls', namespace='app')),
     path(F'{settings.ADMIN_PANEL_PATH}/', admin.site.urls),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
